5f975ee6cdf749b766d671512943a7249902e620
felipemamore <felipemamore@gmail.com>
2020-03-03 11:59:17 -0300

    atualizando

--- Aula01/teste_dobradura/dobradura.py
+++ Aula01/teste_dobradura/dobradura.py
@@ -1,22 +1,14 @@
 class Dobradura:
    def get_dobrar(self, numero_dobradura):
-        self.numero_dobradura = 0
-        self.dobradura = []
-
-        if numero_dobradura < 0:
-            return f'Digite um numero positivo'
+        self.numero_dobradura = 1
+        if type(numero_dobradura) == int and numero_dobradura > 0:
+            for i in range(0,numero_dobradura):
+                self.numero_dobradura = 2**numero_dobradura
+            return self.numero_dobradura
         else:
-            if type(numero_dobradura) != int:
-                return f'Digite apenas numeros!'
+            return False
 
-            else:
-                for i in range(self.numero_dobradura):
-                    self.dobras = 2 ** i
-                    self.dobradura.append(self.dobras)
-            return f'Qtde de dobraduras no papel: {self.dobradura}'
 
-if __name__== '__main__':
-    dobras=Dobradura()
 
 
 
---
+++ Aula02/Classe_Pessoa.py
@@ -0,0 +1,69 @@
+class Pessoa:
+    def __init__(self, nome, idade, ocupacao, rua, bairro, numero, cidade):
+
+        self.__nome = nome
+        self.__idade = idade
+        self.__ocupacao = ocupacao
+        self.endereco=Endereco(rua, bairro, numero, cidade)
+
+
+    def setNome(self, nome):
+        self.__nome = nome
+    def setIdade(self,idade):
+        self.__idade = idade
+    def setOcupacao(self,ocupacao):
+        self.__ocupacao = ocupacao
+    def getNome(self):
+        return self.nome
+    def getIdade(self):
+        return self.idade
+    def getOcupacao(self):
+        return self.ocupacao
+
+class Endereco:
+    def __init__(self, rua, bairro, numero, cidade):
+        self.__rua = rua
+        self.__bairro = bairro
+        self.__numero = numero
+        self.__cidade = cidade
+    def setRua(self, rua):
+        self.__rua = rua
+    def setBairro(self, bairro):
+        self.__bairro = bairro
+    def setNumero(self, numero):
+        self.__numero = numero
+    def setCidade(self, cidade):
+        self.__cidade = cidade
+    def getRua(self):
+        return self.rua
+    def getBairro(self):
+        self.bairro
+    def getNumero(self):
+        return self.numero
+    def getCidade(self):
+        return self.cidade
+
+class Engenheiro(Pessoa):
+    def __init__(self,nome, idade, ocupacao, categoria, registro):
+        super().__init__(nome, idade, ocupacao)
+        self.__categoria=categoria
+        self.__registro=registro
+    def setCategoria(self, categoria):
+        self.__categoria = categoria
+    def setRegistro(self,registro):
+        self.__registro=registro
+    def getCategoria(self):
+        return self.categoria
+    def getRegistro(self):
+        return self.registro
+e=Engenheiro()
+e.setCategoria()
+
+
+
+
+
+
+
+
+
